0937d78163aa60bf8b728dcea2c838006c85081a
gintarevk <gintare.v.k@gmail.com>
2023-02-20 15:58:02 +0200

    Add new Driver class

---
+++ src/main/java/lt/gintare/seleniumeasy/pom/utils/Drivers.java
@@ -0,0 +1,34 @@
+package lt.gintare.seleniumeasy.pom.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Drivers {
+
+    private static WebDriver driver;
+
+    public static void setDriver() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--start-maximized ");
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.close();
+    }
+}
