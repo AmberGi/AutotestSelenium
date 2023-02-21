aabd5da3f60739870ac214890eba82a8717c8ad4
gintarevk <gintare.v.k@gmail.com>
2023-02-21 09:08:36 +0200

    Rename BasicFirstFormTest class

--- src/main/java/lt/gintare/seleniumeasy/pom/tests/BasicFirstFormTest.java
+++ src/main/java/lt/gintare/seleniumeasy/pom/tests/BasicFirstFormTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class BasicFirstForm {
+public class BasicFirstFormTest {
 
     @BeforeTest
     public void setUpDriver() {
